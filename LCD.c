9999830e55afb754456d62b8051d8f4a55bcfaae
Daniel Joseph Alapat <71063467+daniboi16@users.noreply.github.com>
2021-05-14 10:41:55 +0530

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -1,4 +1,5 @@
-// find the blog along with hardware conections and code explanation at :https://danielalapat.hashnode.dev/stm32-f4-interfacing-lcd
+// find the blog along with hardware conections and code explanation at : https://danielalapat.hashnode.dev/stm32-f4-interfacing-lcd
+// parallel interfacing of LCD
 #include "stm32f4xx.h"
 void configureGPIO(void);
 void msDelay(uint32_t msTime);
@@ -7,7 +8,7 @@ void data(void);
 
 int main(void)
 {
-	configureGPIO();
+	configureGPIO();  
 	msDelay(500);
 	
 	GPIOD->ODR &= ~(0xFFUL<<0); //to make bit 0 to 7 "0"
@@ -47,22 +48,21 @@ int main(void)
 	}
 }
 
-void configureGPIO(void)
+void configureGPIO(void)		//to set GPIO pins as output
 {
-	RCC->AHB1ENR  |= (1UL<<3); //clock for port d
-	GPIOD->MODER &= ~(0x3FFFFFUL<<0); //making sure pins 0 to 10 are "0" 
-	GPIOD->MODER |= (0x155555UL<<0); //making pins 0 to 10 to "01" which is output
+	RCC->AHB1ENR  |= (1UL<<3);		 //clock for port d
+	GPIOD->MODER &= ~(0x3FFFFFUL<<0); 	//making sure pins 0 to 10 are "0" 
+	GPIOD->MODER |= (0x155555UL<<0); 	//making pins 0 to 10 to "01" which is output
 }
 
-void msDelay(uint32_t msTime)
+void msDelay(uint32_t msTime)		//to impliment software delay
 {
-	for (uint32_t i=0;i<msTime*4000;i++)
-	{
-		__NOP();
-	}
+	//"For loop" takes 4 clock cycles to get executed. Clock frequency is 16MHz
+	//16MHz/4=4MHz. If we want 1000ms delay, 4MHz/1000=4000, so we have to multiply by 4000 to get a delay of 1s
+	for (uint32_t i=0;i<msTime*4000;i++);
 }
 
-void command(void)
+void command(void)			//handshake to send commands to LCD
 {
 	GPIOD->ODR &= ~(0x7UL<<8); //to make pin 8,9,10 to "000"
 	GPIOD->ODR |= (0x4UL<<8); //to make pin 8,9,10 to "001"
@@ -71,7 +71,7 @@ void command(void)
 	msDelay(1);
 }
 
-void data(void)
+void data(void)				//handshake to send data to LCD
 {
 	GPIOD->ODR &= ~(0X7UL<<8); //to make pin 8,9,10 to "000"
 	GPIOD->ODR |= (0x5UL<<8);  //to make pin 8,9,10 to "101"
